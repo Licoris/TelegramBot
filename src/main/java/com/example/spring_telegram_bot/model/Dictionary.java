8be8652278de77e3897ede290cc40c03649ad75e
ur licoris <kuzminykh6@gmail.com>
2022-11-08 20:43:33 +0400

    Added spotbugs and checkstyle, code refactoring

--- src/main/java/com/example/spring_telegram_bot/model/Dictionary.java
+++ src/main/java/com/example/spring_telegram_bot/model/Dictionary.java
@@ -1,21 +1,18 @@
 package com.example.spring_telegram_bot.model;
 
-import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.experimental.FieldDefaults;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
 import java.util.List;
 
-@FieldDefaults(makeFinal = true,level =  AccessLevel.PRIVATE)
 @Getter
 @Builder
 @RedisHash("dictionary")
 public class Dictionary {
     @Id
-    String id;
+    private String id;
 
-    List<Word> wordList;
+    private List<Word> wordList;
 }
--- src/main/java/com/example/spring_telegram_bot/model/Word.java
+++ src/main/java/com/example/spring_telegram_bot/model/Word.java
@@ -1,23 +1,20 @@
 package com.example.spring_telegram_bot.model;
 
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.experimental.FieldDefaults;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
 import java.util.Set;
 
-@FieldDefaults(level = AccessLevel.PRIVATE)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @RedisHash("word")
 public class Word {
     @Id
-    String word;
+    private String word;
 
-    Set<String> mistakes;
+    private Set<String> mistakes;
 }
